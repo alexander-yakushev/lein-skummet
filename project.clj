aa31f67fee64648a98d389fc3bf2fd786c41e58b
Alexander Yakushev <alex@bytopia.org>
2015-02-23 16:05:41 +0200

    Version 0.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.skummet/lein-skummet "0.2.0"
+(defproject org.skummet/lein-skummet "0.2.1"
   :description "Leiningen plugin for Project Skummet"
   :url "https://github.com/alexander-yakushev/lein-skummet"
   :dependencies []
