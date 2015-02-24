51051c01a054ec35685a9df50a8ef865b7e8edbc
Alexander Yakushev <alex@bytopia.org>
2015-02-24 09:19:08 +0200

    Update lein-skummet dependency

--- sample/project.clj
+++ sample/project.clj
@@ -2,7 +2,7 @@
   :dependencies [[org.skummet/clojure "1.7.0-alpha5-r1"]
                  [org.clojure/core.async "0.1.303.0-886421-alpha" :exclusions [org.clojure/clojure]]]
   :profiles {:default []}
-  :plugins [[org.skummet/lein-skummet "0.2.0"]]
+  :plugins [[org.skummet/lein-skummet "0.2.1"]]
   :main sample.core
   :aot :all
   :skummet-skip-vars ["#'leantest.core/wont-be-lean"])
