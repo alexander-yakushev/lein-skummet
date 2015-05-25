73a6a1c7e2eb493353f1464d376f4bc448e2ce54
Alexander Yakushev <alex@bytopia.org>
2015-05-25 15:39:11 +0200

    Update sample

--- sample/project.clj
+++ sample/project.clj
@@ -1,6 +1,7 @@
 (defproject lein-skummet-sample "0.0.1-SNAPSHOT"
-  :dependencies [[org.skummet/clojure "1.7.0-alpha5-r1"]
-                 [org.clojure/core.async "0.1.303.0-886421-alpha" :exclusions [org.clojure/clojure]]]
+  :dependencies [[org.skummet/clojure "1.7.0-RC1"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
+  :exclusions [org.clojure/clojure]
   :profiles {:default []}
   :plugins [[org.skummet/lein-skummet "0.2.1"]]
   :main sample.core
