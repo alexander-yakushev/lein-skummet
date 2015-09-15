030c45a1c28fe027b35a73960d22413af6ecc123
Alexander Yakushev <alex@bytopia.org>
2015-09-16 00:48:38 +0300

    Fix uberjar generation in Leiningen 2.5.x
    Fixes #5.

--- src/leiningen/skummet.clj
+++ src/leiningen/skummet.clj
@@ -22,38 +22,41 @@
 
 (defn skummet
   [project & [subtask & args]]
-  (cond
-   (= subtask "compile")
-   (do
-     (leiningen.clean/clean project)
-     (if-let [namespaces (seq (leiningen.compile/stale-namespaces project))]
-       (let [form `(let [lean-var?# (fn [var#] (not (#{~@(:skummet-skip-vars project)}
-                                                    (str var#))))]
-                     (binding [~'clojure.core/*lean-var?* lean-var?#
-                               ~'clojure.core/*lean-compile* true
+  (let [project (-> project
+                    (vary-meta assoc-in [:profiles ::skummet]
+                               {:prep-tasks []
+                                :auto-clean false
+                                :jvm-opts ["-Dclojure.compile.ignore-lean-classes=true"]})
+                    (pr/merge-profiles [::skummet]))]
+    (cond
+      (= subtask "compile")
+      (do
+        (leiningen.clean/clean project)
+        (if-let [namespaces (seq (leiningen.compile/stale-namespaces project))]
+          (let [form `(let [lean-var?# (fn [var#] (not (#{~@(:skummet-skip-vars project)}
+                                                       (str var#))))]
+                        (binding [~'clojure.core/*lean-var?* lean-var?#
+                                  ~'clojure.core/*lean-compile* true
 
-                               ~'clojure.core/*compiler-options*
-                               {:elide-meta [:doc :file :line :added :arglists
-                                             :column :static :author :added]}]
-                       (doseq [namespace# '~namespaces]
-                         (println "Compiling" namespace#)
-                         (compile namespace#))))
-             project (-> project
-                         (update-in [:prep-tasks] (partial remove #{"compile"}))
-                         (assoc :jvm-opts ["-Dclojure.compile.ignore-lean-classes=true"]))]
-         (try (eval-in-project project form)
-              (catch Exception e
-                (main/abort "Compilation failed:" (.getMessage e)))))
-       (main/debug "All namespaces already AOT compiled.")))
+                                  ~'clojure.core/*compiler-options*
+                                  {:elide-meta [:doc :file :line :added :arglists
+                                                :column :static :author :added]}]
+                          (doseq [namespace# '~namespaces]
+                            (println "Compiling" namespace#)
+                            (compile namespace#))))]
+            (try (eval-in-project project form)
+                 (catch Exception e
+                   (main/abort "Compilation failed:" (.getMessage e)))))
+          (main/debug "All namespaces already AOT compiled.")))
 
-   (= subtask "run")
-   (let [deps-line (->> (cp/resolve-dependencies :dependencies project)
-                        (cons (jio/file (:target-path project) "classes"))
-                        (interpose ":") (apply str))]
-     (apply eval/sh "java" "-cp" deps-line (str (:main project)) args))
+      (= subtask "run")
+      (let [deps-line (->> (cp/resolve-dependencies :dependencies project)
+                           (cons (jio/file (:target-path project) "classes"))
+                           (interpose ":") (apply str))]
+        (apply eval/sh "java" "-cp" deps-line (str (:main project)) args))
 
-   (= subtask "jar")
-   (leiningen.uberjar/uberjar project)
+      (= subtask "jar")
+      (leiningen.uberjar/uberjar project)
 
-   :else
-   (main/abort "Wrong subtask:" subtask)))
+      :else
+      (main/abort "Wrong subtask:" subtask))))
