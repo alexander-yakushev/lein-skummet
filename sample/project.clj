3abe0abb45ed126526d19e0f0303e6972c06aa42
Alexander Yakushev <alex@bytopia.org>
2015-09-16 00:49:43 +0300

    Version 0.2.2

--- sample/project.clj
+++ sample/project.clj
@@ -1,9 +1,9 @@
 (defproject lein-skummet-sample "0.0.1-SNAPSHOT"
-  :dependencies [[org.skummet/clojure "1.7.0-RC1"]
+  :dependencies [[org.skummet/clojure "1.7.0-r1"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
   :exclusions [org.clojure/clojure]
   :profiles {:default []}
-  :plugins [[org.skummet/lein-skummet "0.2.1"]]
+  :plugins [[org.skummet/lein-skummet "0.2.2"]]
   :main sample.core
   :aot :all
   :skummet-skip-vars ["#'leantest.core/wont-be-lean"])
